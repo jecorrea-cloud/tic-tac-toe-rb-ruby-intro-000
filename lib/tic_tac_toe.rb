2ca84debb8c00cc89e2239121b91fcf3cacc3b51
Joaquin Correa <jecorrea@protonmail.com>
2021-04-01 23:27:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -115,6 +115,6 @@ def play(board)
     winner(board) == "X" ||  winner(board) == "O"
     puts "Congratulations #{winner(board)}!"
   else draw?(board)
-    puts "Cats's Game!"
+    puts "Cat's Game!"
   end
 end
